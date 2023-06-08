72753d1234b6313d22052b97abbf6550110fd98f
李芳军 <lifangjun@lifangjundeMacBook-Pro.local>
2023-06-09 00:00:10 +0800

    feat

--- vue-demo/src/components/HelloWorld.vue
+++ vue-demo/src/components/HelloWorld.vue
@@ -10,18 +10,57 @@
   <div class="btn">
     <button @click="btn">点击</button>
   </div>
+  <table class="info">
+    <tr>
+        <th>姓名</th>
+        <th>年龄</th>
+        <th>性别</th>
+        <th>爱好</th>
+    </tr>
+    <tr v-for="(item, index) in info" :key="index">
+        <td>{{item.name}}</td>
+        <td>{{item.age}}</td>
+        <td>{{item.sex}}</td>
+        <td>{{item.hobby}}</td>
+    </tr>
+  </table>
 </template>
 
 <script>
+import {ref, reactive} from 'vue'
 export default {
     setup() {
-        let msg = "你好"
-        const arr = [1, 2, 3, 4]
+        let msg = ref("你好")
+        const arr = ref([1, 2, 3, 4])
+        const info = reactive([
+            {
+                name: "张三",
+                age: 18,
+                sex: "男",
+                hobby: ['打篮球', '游泳', '吹牛逼']
+            },
+            {
+                name: "李四",
+                age: 28,
+                sex: "男",
+                hobby: ['打游戏', '聊天', '吹牛逼']
+            },
+            {
+                name: "梅艳芳",
+                age: 34,
+                sex: "女",
+                hobby: ['拍电影', '喝酒', '唱歌']
+            },
+        ])
         function btn() {
-            msg = "牛逼"
-            console.log(msg);
+            if (msg.value==='你好') {
+                msg.value = "牛逼"
+            }else {
+                msg.value = "你好"
+            }
+            arr.value = [5, 6, 7, 8]
         }
-        return { msg, arr, btn }
+        return { msg, arr, btn, info }
     }
 }
 </script>
