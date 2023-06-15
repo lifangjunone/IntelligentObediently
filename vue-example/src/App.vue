6210de7b32dca8995d4ef0b11bac3adf16354ec4
lifangjun <lifj@ebondhm.com>
2023-06-15 17:08:08 +0800

    feat

---
+++ vue-example/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <HelloWorld :titles="titles" @changeItem="change"></HelloWorld>
+  <p>{{ contents[currIndex] }}</p>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+
+export default {
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      currIndex: 0,
+      titles: ['鞋子', '衣服', '袜子'],
+      contents: ['鞋子内容', '衣服内容', '袜子内容']
+    }
+  },
+  methods: {
+    change(index) {
+      this.currIndex = index
+    }
+  }
+}
+
+
+</script>
+
+<style>
+p {
+  height: 400px;
+  text-align: center;
+  line-height: 400px;
+  font-size: 35px;
+}
+</style>
---
+++ vue-example/src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="navbar">
+    <div 
+      class="navbar-item"
+      v-for="(item, index) in titles" 
+      :class="{ active: currentIndex === index}" 
+      :key="item"
+      @click="clickItem(index)">
+      <!-- 动态绑定 class -->
+      <span>{{ item }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  props: ['titles'],
+  emits: ['changeItem'],
+  data() {
+    return {
+      currentIndex: Number
+    }
+  },
+  methods: {
+    clickItem(index) {
+      this.currentIndex = index
+      this.$emit('changeItem', index)
+    },
+
+  }
+}
+
+</script>
+
+<style scoped>
+
+.navbar {
+  display: flex;
+  margin-top: 15px;
+  font-size: 25px;
+}
+
+.navbar .navbar-item {
+  flex: 1;
+  text-align: center;
+}
+
+.active  span{
+  color: red;
+  border-bottom: 3px solid red;
+}
+
+</style>
